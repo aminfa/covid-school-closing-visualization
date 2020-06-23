cb6e341ab0345a97047b5722773f2e0332c7106a
Florian <florianutt@live.de>
2020-06-23 10:45:31 +0200

    basic version

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ authors:
 
 #imports
 import pandas as pd
+import plotly.express as px
 
 #read in files
 dfCovid = pd.read_csv("Data/owid-covid-data.csv", delimiter=",")
 dfSchools = pd.read_csv("Data/school-closures-covid.csv", delimiter=",")
 #drop columns
-CovidCloumnList = ["iso_code","continent","location","date","total_cases_per_million"]
+CovidCloumnList = ["iso_code","continent","location","date","new_cases_per_million"]
 dfCovid = dfCovid[CovidCloumnList]
 # remove international and worls rows
 dfCovid = dfCovid[dfCovid.location != "International"]
@@ -25,5 +26,30 @@ dfCovid["date"] = pd.to_datetime(dfCovid.date)
 dfSchools.rename(columns={"Code": "iso_code","Date": "date"}, inplace = True)
 #join dataframes
 dfTotal = pd.merge(dfCovid, dfSchools,  on=["iso_code","date"])
+#remove nan
+dfTotal["new_cases_per_million"] = dfTotal["new_cases_per_million"].fillna(0)
+dfTotal["new_cases_per_million"] = dfTotal["new_cases_per_million"].astype(str)
+dfTotal["new_cases_per_million"] = dfTotal["new_cases_per_million"].str.replace("-","")
+dfTotal["date"] = dfTotal["date"].astype(str)
+dfTotal["new_cases_per_million"] = dfTotal["new_cases_per_million"].astype(float)
+#dfTotal["School closures"] = dfTotal["School closures"].astype(str)
 #save preprocessed and merged data
 dfTotal.to_csv("Data/covidMerged.csv", sep=',')
+print(dfTotal.dtypes)
+
+#working example
+'''
+df = px.data.gapminder()
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+fig.show()
+
+
+'''
+fig = px.scatter_geo(dfTotal, locations="iso_code", color="School closures",
+                     hover_name="location", size="new_cases_per_million",
+                     animation_frame="date",
+                     projection="natural earth")
+fig.show()
