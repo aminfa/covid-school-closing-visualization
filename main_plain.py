f74e655c62811a28f2557b59c430ec9978d6c8b8
Amin <aminfaez@mail.upb.de>
2020-06-26 15:05:58 +0200

    Added color column

--- main_plain.py
+++ main_plain.py
@@ -19,8 +19,6 @@ adjust color scale to visualization -> Floriam
 add title to visualization -> Florian
 '''
 
-
-
 # imports
 import pandas as pd
 import plotly.express as px
@@ -42,7 +40,7 @@ with open("Data/owid-covid-data.csv", "r") as covid_csv:
     for row in reader:
         body.append(row)
 
-needed_columns = ["iso_code","continent","location","date","new_cases_per_million"]
+needed_columns = ["iso_code", "continent", "location", "date", "new_cases_per_million"]
 needed_columns_index = []
 
 for nc in needed_columns:
@@ -69,7 +67,6 @@ with open("Data/school-closures-covid.csv", "r") as school_csv:
         body.append(row)
 
 school_data = body
-
 
 # Apply join on iso_code and date.
 # i.e. as SQL:
@@ -114,84 +111,124 @@ for row_school in school_data:
             # add school closure to `data`
             rows_with_matching_date.append(row_school[column_index_closures_school])
 
-
 print("Joined data (first three rows):\n{}\n".format(data[:3]))
 del head, body, school_data
 
-columns = ["ISO-code", "Continent", "Location", "Date", "New Cases per Million", "School Status"]
 
-# Now remove the rows where we dont have school closures
-rows_remaining = []
-for i, row in enumerate(data):
-    # If the length of the row stayed the same after the join operation then the school closure information is missing
-    if len(row) == len(columns):
-        rows_remaining.append(row)
-    else:
-        row.append(0)
-        rows_remaining.append(row)
+columns = ["ISO-code", "Continent", "Location", "Date", "New Cases per Million", "school_status_code", "circle_size", "School Status"]
 
-print("There are {} many rows without school closure information that are removed.".format(len(data) - len(rows_remaining)))
+# This function defines the translation between color code and color text:
+color_status_texts = ["No Data", "No Restriction Measures", "Recommended Closing", "Required Closing at Some Levels" ,"Required Closing at All Levels"]
+def get_circle_color(color_code):
+    if color_code is None or color_code == "":
+        return "No Data"
+    color_code = str(color_code)
+    if  color_code == "0":
+        return "No Restriction Measures"
+    elif color_code == "1":
+        return "Recommended Closing"
+    elif color_code == "2":
+        return "Required Closing at Some Levels"
+    elif color_code == "3":
+        return "Required Closing at All Levels"
 
-data = rows_remaining
+first_date = None
 
 # Lets format the columns
 rows_remaining = []
+logs = {}
 for row in data:
     try:
+        if len(row) == 5:
+            # School status is missing:
+            row.append("")  # None signals missing value
+        elif len(row) != 6:
+            # Row length is weird:
+            raise Exception("illegal-row-dimension")
+
         # Filter empty values
-        for value in row[:-1]:
+        for value in row[:-2]:
             if value is None or len(value) == 0:
-                # raise Exception("Empty value")
-                # TODO write as log file
+                raise Exception("Empty value")
                 pass
 
         # format date.
         # It is present as a string like `2019-12-31`
         # Transform it to: Dec, 31
         row_date = row[3]
-        row[3] = datetime.strptime(row_date,  "%Y-%m-%d").strftime("%b %d")
+        date = datetime.strptime(row_date, "%Y-%m-%d")
+        row[3] = date.strftime("%b %d")
 
         # format new cases
         new_cases = row[4]
         if len(new_cases) == 0:
-            new_cases = '0' # assume its 0
-        new_cases_float = float(new_cases) # parse the float
+            new_cases = '0'  # assume its 0
+        new_cases_float = float(new_cases)  # parse as float
         if new_cases_float < 0:
             raise Exception("Negative value")
         # add 100 to all new cases:
-        new_cases_float += 100
         row[4] = new_cases_float
 
         # format School Status
-        school = row[5]
-        school_int = int(school) # parse the float
-        if school_int not in [0, 1, 2, 3]:
-            raise Exception("Illegal school status")
-        row[5] = school_int
+        # school = row[5]
+        # school_int = None
+        # if school is not None:
+        #     school_int = int(school)  # parse the text
+        # if school_int not in [None, 0, 1, 2, 3]:
+        #     raise Exception("Illegal school status")
+        # row[5] = school_int
+
+        # append circle size:
+        row.append(100)
+
+        # append circle color:
+        row.append(get_circle_color(row[5]))
 
         rows_remaining.append(row)
     except Exception as ex:
         print("Couldn't format row: `{}`\n Error:".format(row), ex)
         # traceback.print_exc()
+        message = str(ex)
+        if message not in logs:
+            logs[message] = []
+        logs[message].append(row)
+
+# save deleted rows:
+for message in logs.keys():
+    with open("Data/deleted{}.csv".format(message), "w") as log_f:
+        wr = csv.writer(log_f, quoting=csv.QUOTE_ALL)
+        wr.writerows(logs[message])
 
 data = rows_remaining
+with open("Data/resulting_table.csv", "w") as result_f:
+    wr = csv.writer(result_f, quoting=csv.QUOTE_ALL)
+    wr.writerows(data)
+
 
 # Lets transform data to a dataframe and present it using plotly
 df = pd.DataFrame(data, columns=columns)
-fig = px.scatter_geo(df, locations=columns[0], color=columns[5],
-                     hover_name=columns[2],
-                     size=columns[4],
-                     animation_frame=columns[3],
+
+hover_columns = ["Location", "Date", "New Cases per Million", "School Status"]
+info_box_values = df[hover_columns]
+fig = px.scatter_geo(df,
+                     locations=columns[0],  # map position (iso code)
+                     size=columns[-2],  # circle size
+                     color=columns[-1],  # circle color
+                     # color_discrete_sequence=color_status_texts,
+                     hover_name=columns[2],  # Location
+                     hover_data=info_box_values,
+                     # location, date, new cases per million, circle color # TODO change column name
+                     animation_frame=columns[3],  # Date
+                     size_max=10,  # circle max size
                      projection="natural earth")
 fig.update_layout(
     title={
         'text': "Measures on educational institutes in combination with sum of new cases per million in the last 7 days",
-        'y':1,
-        'x':0.5,
+        'y': 1,
+        'x': 0.5,
         'xanchor': 'center',
         'yanchor': 'top'
-        }
-    )
-    
-fig.show()
+    }
+)
 
+fig.show()
